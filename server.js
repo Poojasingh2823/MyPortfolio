7b74724c4b1a544e63733cecb1bad286fe00bb3f
Poojasingh2823 <160371302+Poojasingh2823@users.noreply.github.com>
2024-06-26 11:06:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const path = require("path")
+import express, {static as static_, json} from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { join } from "path";
 
 //dotenv configuartion
-dotenv.config();
+config();
 
 //rest object
 const app = express();
 
 //midlewares
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 //Static Files
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(static_(join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
 app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+  res.sendFile(join(__dirname,'./client/build/index.html'))
 })
 
 //port
