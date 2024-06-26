f0557296d4e174816a05362fb49687115b83f34b
pooja singh <simranpooja.567r@gmail.com>
2024-06-26 10:18:39 +0530

    PortfolioWebsite
    This is mern portfolio

---
+++ controllers/portfolioController.js
@@ -0,0 +1,53 @@
+const mailer = require("nodemailer");
+
+
+//transport
+const transporter = mailer.createTransport({
+  service:'gmail',
+  auth:{
+      user:'simranpooja.567r@gmail.com',
+      pass:'ycfa dyjy ceka ecqv',
+  },
+})
+
+const sendEmailController = (req, res) => {
+  try {
+    const { name, email, msg } = req.body;
+
+    //validation
+    if (!name || !email || !msg) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide All Fields",
+      });
+    }
+    //email matter
+    transporter.sendMail({
+      to: "simranpooja.567r@gmail.com",
+      from: "simranpooja.567r@gmail.com",
+      subject: "Regarding Mern Portfolio App",
+      html: `
+        <h5>Detail Information</h5>
+        <ul>
+          <li><p>Name : ${name}</p></li>
+          <li><p>Email : ${email}</p></li>
+          <li><p>Message : ${msg}</p></li>
+        </ul>
+      `,
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Your Message Send Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Send Email API Error",
+      error,
+    });
+  }
+};
+
+module.exports = { sendEmailController };
